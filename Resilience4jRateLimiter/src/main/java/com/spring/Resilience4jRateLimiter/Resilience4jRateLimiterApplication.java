a1ca34c03eed0b9267f79e05586b433535f6b19f
SaiAshish <saiashish3760@gmail.com>
2023-01-19 04:22:12 +0530

    Update Resilience4jRateLimiterApplication.java

--- Resilience4jRateLimiter/src/main/java/com/spring/Resilience4jRateLimiter/Resilience4jRateLimiterApplication.java
+++ Resilience4jRateLimiter/src/main/java/com/spring/Resilience4jRateLimiter/Resilience4jRateLimiterApplication.java
@@ -2,12 +2,19 @@ package com.spring.Resilience4jRateLimiter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class Resilience4jRateLimiterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Resilience4jRateLimiterApplication.class, args);
+		int i=1;
+		IntStream.range(i,10).parallel().forEach(t->{
+			String response = new RestTemplate().getForObject("http://localhost:8080/order", String.class);
+		});
 	}
 
 }
